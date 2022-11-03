60e058db043c3e4c8e3fd9c03fd6460414d10ce5
marcelo-ls-reis <marcelo.reis5@fatec.sp.gov.br>
2022-11-03 20:09:10 +0000

    atualização

---
+++ app/components/main/main.js
@@ -0,0 +1,21 @@
+
+export default function Main() {
+    return (
+        <>
+            <div>
+                <h3>PROJETOS</h3>
+                <table className="table table-striped" style={{ marginTop: 20 }}>
+                    <thead>
+                        <tr>
+                            <th>NOME</th>
+                            <th>STATUS</th>
+                            <th>AÇÃO</th>
+                        </tr>
+                    </thead>
+                    {/* <tbody>{recordList()}</tbody> */}
+                </table>
+            </div>
+
+        </>
+    )
+}
